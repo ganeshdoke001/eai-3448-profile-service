d6b05f44f84b01f9297f46e84924c75de407a1fd
ganeshdoke001 <ganeshdoke1999@gmail.com>
2024-09-30 10:00:02 +0530

    v1:3448, created profile project

---
+++ src/main/java/com/inn/profile/Eai3448ProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inn.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eai3448ProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eai3448ProfileServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inn/profile/Eai3448ProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eai3448ProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
